9c07682145f27735494747e602377bc2b5f9874b
Afeez Adeboye <afeezadeboyee@gmail.com>
2023-03-19 14:22:26 +0100

    you must work again oooo

--- jenkins-deployment/EKS/variables.tf
+++ jenkins-deployment/EKS/variables.tf
@@ -3,6 +3,3 @@ variable "private_subnet_cidr_blocks" {}
 variable "public_subnet_cidr_blocks" {}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "aws region" {
-  "us-east-1"
-}
