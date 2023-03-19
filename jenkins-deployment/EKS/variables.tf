e429d8fa931df3cbdc751f3adfabfea804e2ae87
Afeez Adeboye <afeezadeboyee@gmail.com>
2023-03-19 15:03:44 +0100

    work

--- jenkins-deployment/EKS/variables.tf
+++ jenkins-deployment/EKS/variables.tf
@@ -1,5 +1,5 @@
 variable "vpc_cidr_block" {}
 variable "private_subnet_cidr_blocks" {}
 variable "public_subnet_cidr_blocks" {}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
